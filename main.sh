8cdf57d88aeb13b5912a7d966c59c73e9ac2d15b
neonaltz <48682189+neonaltz@users.noreply.github.com>
2024-03-20 23:08:59 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -153,13 +153,13 @@ cat >~/3proxy/3proxy.cfg <<END
 #!/bin/bash
 
 daemon
-maxconn 100
+maxconn 10000
 nserver 1.1.1.1
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
-stacksize 6000
+stacksize 60000
 flush
 END
 
